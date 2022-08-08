c3f809d868bb1d454d4baa1e4f4513bc4c5268a2
Aditya Anil Mhaske <adityamhaske711@gmail.com>
2022-08-08 16:45:47 +0530

    class component changed to functional

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,25 +1,20 @@
-import React, { Component } from "react";
+import React  from "react";
 
 
-export class NewsItem extends Component {
-
-
-
-  render() {
-    let { title, description, imageUrl, newsUrl } = this.props;
+function NewsItem (props) {
     return (
       <div className="px-3 py-2" >
         <div className="card h-85 " style={{ width: "18rem" }}>
           <img
             className="card-img-top h-35"
-            src={imageUrl}
-            alt={description}
+            src={props.imageUrl}
+            alt={props.description}
           ></img>
           <div className="card-body ">
-            <h5 className="card-title ">{title}... </h5>
-            <p className="card-text">{description}...</p>
+            <h5 className="card-title ">{props.title}... </h5>
+            <p className="card-text">{props.description}...</p>
 
-            <a href={newsUrl} target="blank" className="btn btn-sm btn-primary">
+            <a href={props.newsUrl} target="blank" className="btn btn-sm btn-primary">
               Read More
             </a>
           </div>
@@ -27,5 +22,5 @@ export class NewsItem extends Component {
       </div>
     );
   }
-}
+
 export default NewsItem;
