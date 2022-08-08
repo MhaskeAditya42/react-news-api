8564fdfe1b2069528190cd59a27750b9baa1f152
Aditya Anil Mhaske <adityamhaske711@gmail.com>
2022-08-08 17:49:49 +0530

    added fetch api

--- src/components/NewsPage.js
+++ src/components/NewsPage.js
@@ -1,30 +1,41 @@
 import React from "react";
 import Card from "../components/Card";
+import axios from "axios";
 
-const data = require("../data/sampleNews.json");
-
-console.log(data);
+let data = require("../data/sampleNews.json");
 
 const NewsPage = () => {
+  
+  
+
+  
+  const fetchAPI = async () => {
+    let response = await axios.get(
+      "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=a9dab654f8684d0296b16ecb602b122b",
+      
+    );
+    console.log(response.data.articles);
+  };
+  
   return (
-    
-    <div className="container d-flex flex-wrap my-3  ">
-     
-          {data.map((element, index) => {
-            return (
-              <Card
-                key={index}
-                title={element.title.slice(0,45)}
-                description={element.description.slice(0,95)}
-                imageUrl={element.urlToImage}
-                newsUrl={element.url}
-                date={element.publishedAt}
-                source={element.source.name}
-              />
-            );
-          })}
-    </div>
-    
+    <>
+      <div className="container d-flex flex-wrap my-3  ">
+        {data.map((element, index) => {
+          return (
+            <Card
+              key={index}
+              title={element.title.slice(0, 45)}
+              description={element.description.slice(0, 95)}
+              imageUrl={element.urlToImage}
+              newsUrl={element.url}
+              date={element.publishedAt}
+              source={element.source.name}
+            />
+          );
+        })}
+      </div>
+      <button onClick={fetchAPI}>call api</button>
+    </>
   );
 };
 export default NewsPage;
